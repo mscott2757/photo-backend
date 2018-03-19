21108f2ae5c241940d5a734079b0446eed74464f
mscott2757 <mscott2757@berkeley.edu>
2018-03-19 15:07:51 -0700

    readded validation

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ import { login, validateUser, validateCookie } from '../controllers/users';
 
 router.route('/login').post(login);
 router.route('/validate').post(validateUser);
-//router.use(validateCookie);
+router.use(validateCookie);
 
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
