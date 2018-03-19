2d6bd079e85919a91da296b40c2e7a42e2d81e50
mscott2757 <mscott2757@berkeley.edu>
2018-03-19 16:42:35 -0700

    updated origin...

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/photo');
 
-const whitelist = [process.env.FRONTEND_URI, 'http://localhost:3000'];
+const whitelist = ['https://mason-photo-dash.herokuapp.com', 'http://localhost:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
